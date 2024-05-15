58b0913b33a766cc84f71684e44c90214752cfe4
Bunterninja2609 <joshi2609@icloud.com>
2024-05-15 08:18:46 +0200

    most basic draw function, that future-proofs

--- main.lua
+++ main.lua
@@ -4,3 +4,6 @@ function love.draw()
 end
 function love.update(dt)
 end
+function draw( drawable, x, y, r, width, height, ox, oy, kx, ky )
+    love.graphics.draw( drawable, x, y, r, width / drawable:getWidth() , height / drawable:getHeight() , ox, oy, kx, ky )
+end
