dc3fb4e6c6038e28bcd957cac9e4de0e254c5489
novaalis <novarinoalissa@gmail.com>
2024-05-17 12:01:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ end
 function love.draw()
 end
 function love.update(dt)
+    --test
 end
 function draw( drawable, x, y, r, width, height, ox, oy, kx, ky )
     love.graphics.draw( drawable, x, y, r, width / drawable:getWidth() , height / drawable:getHeight() , ox, oy, kx, ky )
