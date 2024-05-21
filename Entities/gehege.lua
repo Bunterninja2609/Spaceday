dea8484d7b980a9a72a61bc3311874108b70bb11
nqimq <nanalyyse@gmail.com>
2024-05-21 22:16:28 +0200

    Create gehege.lua
    gehege modul

---
+++ Entities/gehege.lua
@@ -0,0 +1,20 @@
+local gehege={
+    x=0,
+    y=0,
+    body=love.physics.newBody(World,gehege.x,gehege.y,"static"),
+    shape = love.physics.newPolygonShape(gehehe.x,gehege.y,gehehe.x+50,gehege.y,gehehe.x+50,gehege.y+50,gehehe.x,gehege.y+50),
+    fixture=love.physics.newFixture(gehege.body,gehege.shape)
+}
+function gehege.load()
+
+end
+
+function gehege.update(dt)
+
+end
+
+function gehege.draw()
+
+end
+
+return gehege
