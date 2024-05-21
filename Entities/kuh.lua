c5d35a85601fa76df3427f64c9451298013cdb94
novaalis <novarinoalissa@gmail.com>
2024-05-21 21:35:28 +0200

    update kuh.lua

--- Entities/kuh.lua
+++ Entities/kuh.lua
@@ -1,21 +1,29 @@
-local kuh={
+ local kuh={
     x=0,
     y=0,
-    body=love.physics.newBody(World,kuh.x,kuh.y,"dynamic"),
-    shape=love.physics.newCircleShape(16),
-    fixture=love.physics.newFixture(kuh.body,kuh.shape)
+    speed=100,
+    direction=1,
+    body = love.physics.newBody(World,kuh.x,kuh.y, "dynamic"),
+    shape = love.physics.newCircleShape(16),
+    fixture = love.physics.newFixture(kuh.body,kuh.shape)
 }
-function kuh.load()
 
+function kuh.load()
 end
 
 function kuh.update(dt)
+kuh.body:applyLinearImpulse(math.cos(kuh.direction)*kuh.speed,math.sin(kuh.direction)*kuh.speed)
+kuh.body:setLinearVelocity(math.cos(kuh.direction)*kuh.speed,math.sin(kuh.direction)*kuh.speed)
 
 end
 
 function kuh.draw()
-
+ zeichneKuh(kuh.x,kuh.y,kuh.shape)
 end
 
+function zeichneKuh(xKuh,yKuh,r)
+ love.graphics.setColor(1,1,1)
+ love.graphics.circle("fill",xKuh,yKuh,r)
+end
 return kuh
 
