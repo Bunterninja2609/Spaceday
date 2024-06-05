d16777dfc9e1d8ef10473351600e5bf3805ea1cc
Bunterninja2609 <joshi2609@icloud.com>
2024-06-05 12:53:43 +0200

    fixed failed merge in kuh.lua

--- Entities/kuh.lua
+++ Entities/kuh.lua
@@ -11,15 +11,6 @@ local kuh={}
     kuh.clickCount=0
     kuh.kuhBild = love.graphics.newImage("Textures/kuh.png")
 
-local kuh={
-    x=0,
-    y=0,
-    speed=100,
-    direction=1,
-    body = love.physics.newBody(World,kuh.x,kuh.y, "dynamic"),
-    shape = love.physics.newCircleShape(16),
-    fixture = love.physics.newFixture(kuh.body,kuh.shape)
-}
 
 function kuh:load()
     
