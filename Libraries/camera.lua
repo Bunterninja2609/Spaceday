40c5e389a61aad9b596ff100d6020b2f66efe3e3
Bunterninja2609 <joshi2609@icloud.com>
2024-06-17 20:44:55 +0200

    Neue library
    Diese library sorgt dafür, dass wir die kamera zoomen und bewegen können. lest euch bitte den code durch.
    
    WICHTIG: die library verändert dadurch die mausposition. dafür haben wir die neuen angaben camera.mouseX und camera.mouseY

---
+++ Libraries/camera.lua
@@ -0,0 +1,17 @@
+camera = {x = 0, y = 0, offsetX = 0, offsetY = 0,mouseX = 0, mouseY = 0, scale = 1}
+function camera.init()
+    love.graphics.push() -- startet den einfluss der Kamera
+    love.graphics.translate(-camera.x*camera.scale + camera.offsetX, -camera.y*camera.scale + camera.offsetY) -- versetzt den Bildschirm
+    love.graphics.scale(camera.scale) -- zoomt rein
+    camera.mouseX = (camera.x + love.mouse:getX()/camera.scale) - camera.offsetX/ camera.scale-- neue x-koordinate der maus nach verschieben und zoomen
+    camera.mouseY = (camera.y + love.mouse:getY()/camera.scale) - camera.offsetY/ camera.scale-- neue y-koordinate der maus nach verschieben und zoomen
+end
+function camera.exit()
+    love.graphics.pop() -- beendet den einfluss der Kamera
+end
+function camera.follow(x, y, isCentered) -- sorgt dafür, dass die Kamera bestimmten Koordinaten folgt
+    camera.x, camera.y = x, y
+    if isCentered then
+        camera.offsetX, camera.offsetY = love.graphics.getWidth()/2, love.graphics.getHeight()/2 -- zentriert die Kamera
+    end
+end
